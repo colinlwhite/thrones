c2e3ffeb52f9ebd6635af12aa67eb8d7e8d9957d
Colin White <clwhite814@gmail.com>
2018-10-02 20:58:22 -0500

    xhr request

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,8 @@
-import {charactersBuilder} from './components/characters.js';
+
+import {getCharacters} from './data/charactersData.js';
 
 const initializeApp = () => {
-    charactersBuilder();
-}
+    getCharacters();
+};
 
 initializeApp();
