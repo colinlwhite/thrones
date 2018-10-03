a136b07fa58dddf51eb5ab1c0141c0bdae7f2578
Colin White <clwhite814@gmail.com>
2018-10-02 20:03:31 -0500

    created json data

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -1,12 +1,7 @@
 import { printToDom } from "../helpers/util.js";
 import {detailsBuilder} from "./detail.js"
 
-const characters = [
-    {id:'character1', name: 'Robin', house: 'Arryn', imageURL: 'https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2017%2F07%2FScreen-Shot-2017-07-11-at-12.36.25-PM.jpg&c=sc&w=736&h=485'},
-    {id:'character2', name: 'Edmure', house: 'Tully', imageURL: 'http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s3/blackfish-tully-1024.jpg'},
-    {id:'character3', name: 'Rickard', house: 'Stark', imageURL: 'https://media1.popsugar-assets.com/files/thumbor/sUpIi0hjSKVgYeaE74-4g7AHiJ8/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/07/24/805/n/1922283/49869b39593b8e77_736624c75976302c2ebe73.51642632_edit_img_facebook_post_image_file_15913477_1500917045.jpg'},
-    {id:'character4', name: 'Doran', house: 'Martell', imageURL: 'https://vignette.wikia.nocookie.net/gameofthrones/images/f/f9/Profile-DoranMartell.png/revision/latest?cb=20170728221257'}
-];
+const characters = [];
 
 const characterClick = (e) => {
     const characterId = e.target.closest('.character-card').id;
